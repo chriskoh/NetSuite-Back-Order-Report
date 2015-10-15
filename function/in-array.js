c6c24ed86801a96aa8e94d1a34b5e2c1b46e59d4
Chris Koh <chris.koh90@gmail.com>
2015-10-15 10:44:22 -0700

    Create in-array.js

---
+++ function/in-array.js
@@ -0,0 +1,7 @@
+/*-------------------------------------------------------------------------------------------------
+Function: isInArray(value, array)
+Purpose:  Search for value in array
+-------------------------------------------------------------------------------------------------*/
+function isInArray(value, array){
+	  return array.indexOf(value) > -1;
+}
